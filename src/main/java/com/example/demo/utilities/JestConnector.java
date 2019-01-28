0a43972e95de3b84633b6f0e8adf32092de312a1
oyo <oyo@Administrators-MacBook-Pro-38.local>
2019-01-28 19:52:09 +0530

    final

--- src/main/java/com/example/demo/utilities/JestConnector.java
+++ src/main/java/com/example/demo/utilities/JestConnector.java
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class JestConnector {
 
-//    @Value("${elasticsearch.url}")
-//    private String esURL;
+       @Value("${elasticsearch.url}")
+       private String esURL;
 
     public JestClient makeJestClient() {
 
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(new HttpClientConfig
-                .Builder("http://localhost:9200").multiThreaded(true).defaultMaxTotalConnectionPerRoute(2).maxTotalConnection(20).build());
+                .Builder(esURL).multiThreaded(true).defaultMaxTotalConnectionPerRoute(2).maxTotalConnection(20).build());
         JestClient client = factory.getObject();
 
         return client;
